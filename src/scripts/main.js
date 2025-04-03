eecd723e7df4d811b2ce5127fdb08c7e6a90fe87
Emmanemzz <emmarondinelli.ts@gmail.com>
2025-04-03 20:45:16 +0200

    Menu hamburguer

---
+++ src/scripts/main.js
@@ -0,0 +1,16 @@
+var nav;
+var navIcon;
+window.onload = () => {
+    console.log("Documento listo");
+    nav = document.querySelector("#mobile-nav");
+    navIcon = document.querySelector("#nav-icon");
+}
+
+function onMenuClick() {
+    nav.classList.toggle("hidden");
+    if(nav.classList.contains("hidden")){
+        navIcon.src = "./assets/images/menu/menu.png";
+    }else{
+        navIcon.src = "./assets/images/menu/close.png"; 
+    }
+}
